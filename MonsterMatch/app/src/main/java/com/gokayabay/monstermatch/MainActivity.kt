763e7abbaac3c67da6bcb52065d3cf6b2884da1f
Gökay Abay <abaygokay@gmail.com>
2021-01-28 11:16:12 -0800

    gradle view binding added

--- MonsterMatch/app/src/main/java/com/gokayabay/monstermatch/MainActivity.kt
+++ MonsterMatch/app/src/main/java/com/gokayabay/monstermatch/MainActivity.kt
@@ -2,10 +2,17 @@ package com.gokayabay.monstermatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gokayabay.monstermatch.databinding.ActivityMainBinding
 
+private lateinit var binding: ActivityMainBinding
+
+// entry point to the application
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setting the contect view as this layout
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.headImageView.setImageResource(R.drawable.monster2_head)
     }
 }
